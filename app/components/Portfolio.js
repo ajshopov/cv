0f36ee5b968d6596470d83a055d65c6686caab0c
ajshopov <alexanderjshopov@gmail.com>
2019-06-30 17:30:00 +1000

    set up projects

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Project from "./Project";
+import projectData from "../../lib/projectData";
 
-const Portfolio = () => (
-  <div>
-    Portfolio
-  </div>
-);
+class Portfolio extends React.Component {
+  state = {
+    projects: projectData,
+    selectedProjects: undefined
+  }
+
+  render(){
+    console.log(this.state.projects)
+    return (
+      <div>
+        Portfolio
+        {this.state.projects.map((project, index) =>
+          <Project key={index} projectText={project.name}/>
+        )}
+      </div>
+    )
+  }
+}
+
 
 export default Portfolio;
---
+++ app/components/Project.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Project = (props) => (
+  <div>
+    #Project
+    <p>{props.projectText}</p>
+  </div>
+);
+
+export default Project;
