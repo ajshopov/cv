6666612776957e467b2faba884eca03c1e8fad01
ajshopov <alexanderjshopov@gmail.com>
2019-07-15 15:00:22 +1000

    changed to hash router

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => (
   <div>
     Home
+    <p>Web developer intro</p>
   </div>
 );
 
--- app/routers/AppRouter.js
+++ app/routers/AppRouter.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Header from "../components/Header";
 import Home from "../components/Home";
 import Portfolio from "../components/Portfolio";
 
 const AppRouter = () => (
-  <BrowserRouter basename="/cv">
+  <HashRouter>
     <Header />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/portfolio" component={Portfolio} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AppRouter;
