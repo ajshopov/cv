8d59bb7107cffd8c1a14a414b34e2a7ad6b0595b
ajshopov <alexanderjshopov@gmail.com>
2019-07-18 17:00:43 +1000

    added json file for links, added icons

--- app/components/Links.js
+++ app/components/Links.js
@@ -1,44 +1,30 @@
 import React from "react";
+import Urls from "../../lib/linksPage";
 import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Typography from '@material-ui/core/Typography';
 import Container from "@material-ui/core/Container";
+import Label from '@material-ui/icons/LabelImportant';
 
 const Links = () => (
   <Container>
     <Typography gutterBottom>
-      A collection of links to useful tools, resources and articles that I've found useful.
+      A collection of links to resources, tools and articles that I've found useful.
     </Typography>
-    <Typography>
       <List>
-        <ListItem>
-          <Link href="https://frontendmasters.com/books/front-end-handbook/2019/">
-            Front-end Developer Handbook 2019 by Cody Lindley
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="https://medium.com/@js_tut/the-complete-css-flex-box-tutorial-d17971950bdc">
-            The Complete CSS Flex Box Tutorial
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="https://practicaltypography.com/summary-of-key-rules.html">
-            Butterick’s Practical Typography - Summary of key rules
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="https://picular.co/">
-            Picular - Google, but for colors.
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="https://frontendmasters.com/books/front-end-handbook/2019/">
-            Front-end Developer Handbook 2019 by Cody Lindley
-          </Link>
-        </ListItem>
+        {Urls.map((listing, index) => {
+          return(
+            <ListItem key={index}>
+              <ListItemIcon><Label /></ListItemIcon>
+              <Link href={listing.linkUrl} rel="noopener noreferrer" target="_blank">
+                {listing.title}
+              </Link>
+            </ListItem>
+          )
+        })}
       </List>
-    </Typography>
   </Container>
 );
 
