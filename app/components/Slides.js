64c151a3853e8f41650ba671ac7652eeca7594e3
ajshopov <alexanderjshopov@gmail.com>
2020-04-19 20:22:59 +1000

    removed fullpage.js will try different plugin

--- app/components/Slides.js
+++ app/components/Slides.js
@@ -1,29 +1,25 @@
 import React from "react";
 import ReactFullpage from '@fullpage/react-fullpage';
 
+// let lastScroll = 0;
+
+// window.addEventListener('scroll', function() {
+//   // scroll down
+//   if (lastScroll < window.pageYOffset) {
+//     window.scrollBy(0, window.innerHeight);
+//   }
+//   // scroll up
+//   else if (lastScroll > window.pageYOffset) {
+//     window.scrollBy(0, window.innerHeight * -1);
+//   }
+//   lastScroll = window.pageYOffset;
+// });
+
 const Slides = () => {
   return(
-    <ReactFullpage
-    //fullpage options
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000} /* Options here */
-
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+    <div>
+        test
+    </div>
   )
 }
 
