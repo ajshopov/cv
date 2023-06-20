fde83d8e10fa57370f3d2ec8dfb638d1b8747166
AL-PC\Alex <aj_shopov@hotmail.com>
2023-06-20 12:58:51 +1000

    added images to grid

--- app/components/Tiles.js
+++ app/components/Tiles.js
@@ -1,8 +1,18 @@
 import React from "react";
 import GridLayout from "react-grid-layout";
+import withStyles from '@mui/styles/withStyles';
+
+const styles = {
+    gridImage: {
+      maxWidth: '100%',
+      maxHeight: '100%'
+    }
+  }
+
 
 class MyFirstGrid extends React.Component {
   render() {
+    const { classes } = this.props
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
       { i: "a", x: 0, y: 0, w: 1, h: 2},
@@ -16,18 +26,28 @@ class MyFirstGrid extends React.Component {
         className="layout"
         layout={layout}
         cols={12}
-        rowHeight={30}
+        rowHeight={60}
         width={1200}
         verticalCompact={false}
       >
-        <div key="a" style={{ border: '1px solid black' }}>a</div>
-        <div key="b" style={{ border: '1px solid black' }}>b</div>
-        <div key="c" style={{ border: '1px solid black' }}>c</div>
-        <div key="d" style={{ border: '1px solid black' }}>d</div>
-        <div key="e" style={{ border: '1px solid black' }}>e</div>
+        <div key="a" style={{ border: '1px solid black' }}>
+            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/id/5546156-L.jpg"/>
+        </div>
+        <div key="b" style={{ border: '1px solid black' }}>
+            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780465066933-L.jpg"/>
+        </div>
+        <div key="c" style={{ border: '1px solid black' }}>
+            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780465066933-L.jpg"/>
+        </div>
+        <div key="d" style={{ border: '1px solid black' }}>
+            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780393339758-L.jpg"/>
+        </div>
+        <div key="e" style={{ border: '1px solid black' }}>
+            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780465066933-L.jpg"/>
+        </div>
       </GridLayout>
     );
   }
 }
 
-export default MyFirstGrid;
+export default withStyles(styles)(MyFirstGrid);
