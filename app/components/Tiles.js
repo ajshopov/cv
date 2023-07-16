ef076c0af5d7a17b4c84d351e81883d76f0f82d9
AL-PC\Alex <alexanderjshopov@gmail.com>
2023-07-16 22:44:05 +1000

    fetch from openlibrary booklist, setup responsive grid layout

--- app/components/Tiles.js
+++ app/components/Tiles.js
@@ -1,5 +1,5 @@
 import React from "react";
-import GridLayout from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
 import withStyles from '@mui/styles/withStyles';
 import "../../node_modules/react-grid-layout/css/styles.css";
 import "../../node_modules/react-resizable/css/styles.css";
@@ -12,45 +12,84 @@ const styles = {
       height: '100%'
     }
   }
-
+  const ResponsiveGridLayout = WidthProvider(Responsive);
 
 class MyFirstGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://openlibrary.org/people/ashopov/books/already-read.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            books: result.reading_log_entries
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  
   render() {
     const { classes } = this.props
-    // layout is an array of objects, see the demo for more complete usage
-    const layout = [
-      { i: "a", x: 0, y: 0, w: 1, h: 2},
-      { i: "b", x: 1, y: 0, w: 3, h: 2, maxH: 6, maxW: 3 },
-      { i: "c", x: 4, y: 0, w: 1, h: 2 },
-      { i: "d", x: 4, y: 0, w: 1, h: 2 },
-      { i: "e", x: 4, y: 0, w: 1, h: 2 },
+    const { error, isLoaded, books } = this.state;
+    const layoutLG = [
+      { i: "0", x: 3, y: 2, w: 3, h: 5},
+      { i: "1", x: 0, y: 0, w: 3, h: 5}
     ];
-    return (
-      <GridLayout
-        className="layout"
-        layout={layout}
-        cols={12}
-        rowHeight={60}
-        width={1200}
-        verticalCompact={false}
-      >
-        <div key="a" style={{ border: '1px solid black' }}>
-            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/id/5546156-L.jpg"/>
+    const layoutMD = [
+        { i: "0", x: 4, y: 0, w: 2, h: 5},
+        { i: "1", x: 1, y: 4, w: 1, h: 1}
+      ];
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div style={{maxWidth: "12000px"}}>
+          {/* <div>
+            {books.map((entry, index) => (
+              <div key={index}>
+                <h2>{entry.work.title}</h2>
+                <p>Author: {entry.work.author_names.join(', ')}</p>
+                <img src={`https://covers.openlibrary.org/b/id/${entry.work.cover_id}-L.jpg`}/>
+              </div>
+            ))}
+          </div> */}
+          <ResponsiveGridLayout
+            layouts={{ lg: layoutLG, md: layoutMD }}
+            rowHeight={60}
+            width={1200}
+            verticalCompact={false}
+            breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          >
+            {books.map((entry, index) => (
+              <div key={index}>
+                <img className={classes.gridImage} src={`https://covers.openlibrary.org/b/id/${entry.work.cover_id}-L.jpg`}/>
+              </div>
+            ))}
+          </ResponsiveGridLayout>
         </div>
-        <div key="b" style={{ border: '1px solid black' }}>
-            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780465066933-L.jpg"/>
-        </div>
-        <div key="c" style={{ border: '1px solid black' }}>
-            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780465066933-L.jpg"/>
-        </div>
-        <div key="d" style={{ border: '1px solid black' }}>
-            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780393339758-L.jpg"/>
-        </div>
-        <div key="e" style={{ border: '1px solid black' }}>
-            <img className={classes.gridImage} src="https://covers.openlibrary.org/b/isbn/9780465066933-L.jpg"/>
-        </div>
-      </GridLayout>
-    );
+      );
+    }
   }
 }
 
